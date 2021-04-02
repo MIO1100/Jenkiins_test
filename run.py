e6fa9f3199a5dcd1d9cc020a66e704a8474833d0
MIO1100 <melnichenko739@mail.ru>
2021-04-02 21:50:01 +0700

    qwe

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import subprocess
 import time
 
-process = subprocess.call("python3 manage.py runserver")
+
+process = subprocess.Popen(['python3', 'manage.py','runserver'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 
 time.sleep(10)
+test = subprocess.Popen(['python3', 'tests/test.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-test = subprocess.call("python3 tests/test.py")
