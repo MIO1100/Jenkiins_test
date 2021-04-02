0543635ac051b279e3e9aeaa050e0fc984d8bc09
MIO1100 <melnichenko739@mail.ru>
2021-04-02 21:11:00 +0700

    qwe

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,18 @@
 import requests
-r = requests.get("http://localhost:8000/",verify=False)
-if r.status_code == requests.codes.ok:
+import time
+
+page = ''
+while page == '':
+    try:
+        page = requests.get("http://localhost:8000/")
+        break
+    except:
+        print("Connection refused by the server..")
+        print("Let me sleep for 5 seconds")
+        print("ZZzzzz...")
+        time.sleep(5)
+        print("Was a nice sleep, now let me continue...")
+        continue
+if page.status_code == requests.codes.ok:
     print("Test OK")
 else: raise Exception("ERROR")
